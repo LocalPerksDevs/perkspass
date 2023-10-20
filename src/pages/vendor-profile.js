0b7fdd6c450445fc7ee1213391983f09c4a65c96
Daniel Whitaker <support@localperks.app>
2023-10-20 09:46:33 -0600

    Filled out the vendor profile

--- src/pages/vendor-profile.js
+++ src/pages/vendor-profile.js
@@ -29,20 +29,34 @@ const VendorProfile = () => {
         if (snapshot.exists) {
             setContact(() => {
                 return {
+                    active: snapshot.data().Active === true ? "Yes" : "No",
                     address: snapshot.data().Address,
-                    name: snapshot.data().Name,
-                    phone: snapshot.data().Phone,
+                    affiliateID: snapshot.data().AffiliateID,
+                    appLaunchDate: !snapshot.data().AppLaunchDate ? "N/A" : snapshot.data().AppLaunchDate.toDate().toDateString(),
                     category: snapshot.data().Category,
-                    typeOfThing: snapshot.data().TypeOfThing,
                     city: snapshot.data().City,
-                    state: snapshot.data().State,
+                    contactEmail: !snapshot.data().ContactEmail ? "N/A" : snapshot.data().ContactEmail,
+                    contactName: !snapshot.data().ContactName ? "N/A" : snapshot.data().ContactName,
+                    contactPhone: !snapshot.data().ContactPhone ? "N/A" : snapshot.data().ContactPhone,
+                    contractEnds: !snapshot.data().ContractEnds ? "N/A" : snapshot.data().ContractEnds.toDate().toDateString(),
+                    disclaimer: !snapshot.data().Disclaimer ? "None" : snapshot.data().Disclaimer,
                     discount: snapshot.data().Discount,
-                    onlineOrdering: snapshot.data().OnlineOrdering === true ? "true" : "false",
-                    website: snapshot.data().Website,
+                    fee: !snapshot.data().Fee ? "N/A" : snapshot.data().Fee,
+                    latLon: snapshot.data().latLon,
+                    logoURL: snapshot.data().LogoURL,
+                    name: snapshot.data().Name,
+                    notes: !snapshot.data().Notes ? "None" : snapshot.data().Notes,
+                    onlineOrdering: snapshot.data().OnlineOrdering === true ? "Yes" : "No",
+                    posCall: !snapshot.data().POSCall ? "N/A" : snapshot.data().POSCall.toDate().toDateString(),
+                    posName: !snapshot.data().POSName ? "N/A" : snapshot.data().POSName,
+                    posSetup: snapshot.data().POSSetup === true ? "Yes" : "No",
+                    phone: snapshot.data().Phone,
                     promoCode: snapshot.data().PromoCode,
-                    disclaimer: snapshot.data().Disclaimer,
-                    contractEnds: snapshot.data().ContractEnds,
-                    logoURL: snapshot.data().LogoURL
+                    state: snapshot.data().State,
+                    termsSigned: snapshot.data().termsSigned === "true" ? "Yes" : "No",
+                    typeOfThing: snapshot.data().TypeOfThing,
+                    website: !snapshot.data().Website ? "None" : snapshot.data().Website,
+                    zip: !snapshot.data().Zip ? "N/A" : snapshot.data().Zip, 
                 };
             });
         }
@@ -75,7 +89,60 @@ const VendorProfile = () => {
                     <div className="col center mt24 mb24">
                         <img src={contact.logoURL} id="logo-img"></img>
                         <p className="vendor-name">{contact.name}</p>
-                        <p className="vendor-category">{contact.category}</p>
+                    </div>
+                    <div className="row space-between">
+                        <div className="col w50 mr12">
+                            <p className="label">CATEGORY</p>
+                            <p className="vendor-category">{contact.category}</p>
+                            <p className="label">CUSTOMER PHONE</p>
+                            <p className="vendor-category">{contact.phone}</p>
+                            <p className="label">CITY</p>
+                            <p className="vendor-category">{contact.city}</p>
+                            <p className="label">ZIP</p>
+                            <p className="vendor-category">{contact.zip}</p>
+                            <p className="label">ACTIVE?</p>
+                            <p className="vendor-category">{contact.active}</p>
+                            <p className="label">CONTACT NAME</p>
+                            <p className="vendor-category">{contact.contactName}</p>
+                            <p className="label">CONTACT EMAIL</p>
+                            <p className="vendor-category">{contact.contactEmail}</p>
+                            <p className="label">REVETIZE FEE</p>
+                            <p className="vendor-category">{contact.fee}</p>
+                            <p className="label">ONLINE ORDERING?</p>
+                            <p className="vendor-category">{contact.onlineOrdering}</p>
+                            <p className="label">POS NAME</p>
+                            <p className="vendor-category">{contact.posName}</p>
+                            <p className="label">POS CALL DATE </p>
+                            <p className="vendor-category">{contact.posCall}</p>
+                            <p className="label">TERMS SIGNED? </p>
+                            <p className="vendor-category">{contact.termsSigned}</p>
+                        </div>
+                        <div className="col w50">
+                            <p className="label">ADDRESS</p>
+                            <p className="vendor-category">{contact.address}</p>
+                            <p className="label">TYPE</p>
+                            <p className="vendor-category">{contact.typeOfThing}</p>
+                            <p className="label">STATE</p>
+                            <p className="vendor-category">{contact.state}</p>
+                            <p className="label">DISCOUNT</p>
+                            <p className="vendor-category">{contact.discount}</p>
+                            <p className="label">APP LAUNCH DATE</p>
+                            <p className="vendor-category">{contact.appLaunchDate}</p>
+                            <p className="label">CONTACT PHONE</p>
+                            <p className="vendor-category">{contact.contactPhone}</p>
+                            <p className="label">CONTRACT ENDS</p>
+                            <p className="vendor-category">{contact.contractEnds}</p>
+                            <p className="label">NOTES</p>
+                            <p className="vendor-category">{contact.notes}</p>
+                            <p className="label">ONLINE ORDERING FORM</p>
+                            <p className="vendor-category">{contact.website}</p>
+                            <p className="label">POS SETUP?</p>
+                            <p className="vendor-category">{contact.posSetup}</p>
+                            <p className="label">PROMO CODE</p>
+                            <p className="vendor-category">{contact.promoCode}</p>
+                            <p className="label">DISCLAIMER</p>
+                            <p className="vendor-category">{contact.disclaimer}</p>
+                        </div>
                     </div>
                 </div>
             </div>
